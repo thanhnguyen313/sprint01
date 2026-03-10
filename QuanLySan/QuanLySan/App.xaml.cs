1ca7e68028a54c12f7a25bfbfb894fd689bd445f
thanhnguyen313 <tothanhnguyen2006@gmail.com>
2026-03-10 10:53:21 +0700

    first commit

---
+++ QuanLySan/QuanLySan/App.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows;
+
+namespace QuanLySan
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ QuanLySan/QuanLySan/TiepNhanSanWindow.xaml.cs
@@ -0,0 +1,19 @@
+using System.Windows;
+
+namespace QuanLySan
+{
+    public partial class TiepNhanSanWindow : Window
+    {
+        public TiepNhanSanWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Các event handler rỗng – chỉ để XAML build được
+        private void BtnClose_Click(object sender, RoutedEventArgs e) { }
+        private void BtnThemGia_Click(object sender, RoutedEventArgs e) { }
+        private void BtnXoaDongGia_Click(object sender, RoutedEventArgs e) { }
+        private void BtnLuu_Click(object sender, RoutedEventArgs e) { }
+        private void BtnLamMoi_Click(object sender, RoutedEventArgs e) { }
+    }
+}
