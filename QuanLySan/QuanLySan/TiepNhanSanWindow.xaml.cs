815d2704e5ee6fe033d56690838894fab0aa31c9
Q.Nguyen <quocnguyen5026@gmail.com>
2026-03-10 15:53:11 +0700

    sua lai cac button

--- QuanLySan/QuanLySan/TiepNhanSanWindow.xaml.cs
+++ QuanLySan/QuanLySan/TiepNhanSanWindow.xaml.cs
@@ -11,7 +11,6 @@ namespace QuanLySan
 
         // Các event handler rỗng – chỉ để XAML build được
         private void BtnClose_Click(object sender, RoutedEventArgs e) { }
-        private void BtnThemGia_Click(object sender, RoutedEventArgs e) { }
         private void BtnXoaDongGia_Click(object sender, RoutedEventArgs e) { }
         private void BtnLuu_Click(object sender, RoutedEventArgs e) { }
         private void BtnLamMoi_Click(object sender, RoutedEventArgs e) { }
